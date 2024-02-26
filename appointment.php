42b4a31ea51f3a5e9750a2eb616e619c7b30ce16
unknown <carreondianaaa@gmail.com>
2024-02-26 17:19:32 +0800

    commit

--- appointment.php
+++ appointment.php
@@ -38,8 +38,8 @@
           <li><a class="nav-link scrollto" href="resources.php">Resources</a></li>
           <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Maintenanceasd</a></li>
-              <li><a href="#">Auto Repair</a></li>
+              <li><a href="#">Maintenance</a></li>
+              <li><a href="#">Auto Repairkjfsj</a></li>
               <li><a href="#">Tune Ups</a></li>
               <!-- <li class="dropdown"><a href="#"><span>Services We Offer</span> <i class="bi bi-chevron-right"></i></a> -->
                   <ul> 
